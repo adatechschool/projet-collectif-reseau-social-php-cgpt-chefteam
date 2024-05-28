6b1afeb0b321ce331148f5baf6cc335e1c09561e
Gulbahar57 <158152207+Gulbahar57@users.noreply.github.com>
2024-05-28 16:10:21 +0200

    include

--- Niveau2/login.php
+++ Niveau2/login.php
@@ -10,24 +10,8 @@ session_start();
         <link rel="stylesheet" href="style.css"/>
     </head>
     <body>
-        <header>
-            <img src="resoc.jpg" alt="Logo de notre réseau social"/>
-            <nav id="menu">
-                <a href="news.php">Actualités</a>
-                <a href="wall.php?user_id=<?php echo $user['id']; ?>">Mur</a>
-                <a href="feed.php?user_id=<?php echo $user['id']; ?>">Flux</a>
-                <a href="tags.php?tag_id=<?php echo $user['id']; ?>">Mots-clés</a>
-            </nav>
-            <nav id="user">
-                <a href="#">Profil</a>
-                <ul>
-                    <li><a href="settings.php?user_id=<?php echo $user['id']; ?>">Paramètres</a></li>
-                    <li><a href="followers.php?user_id=<?php echo $user['id']; ?>">Mes suiveurs</a></li>
-                    <li><a href="subscriptions.php?user_id=<?php echo $user['id']; ?>">Mes abonnements</a></li>
-                </ul>
-
-            </nav>
-        </header>
+    
+    <?php include 'header.php'; ?>
 
         <div id="wrapper" >
 
--- Niveau2/subscriptions.php
+++ Niveau2/subscriptions.php
@@ -10,24 +10,9 @@ session_start();
         <link rel="stylesheet" href="style.css"/>
     </head>
     <body>
-        <header>
-            <img src="resoc.jpg" alt="Logo de notre réseau social"/>
-            <nav id="menu">
-                <a href="news.php">Actualités</a>
-                <a href="wall.php?user_id=<?php echo $user['id']; ?>">Mur</a>
-                <a href="feed.php?user_id=<?php echo $user['id']; ?>">Flux</a>
-                <a href="tags.php?tag_id=<?php echo $tag['id']; ?>">Mots-clés</a>
-            </nav>
-            <nav id="user">
-                <a href="#">Profil</a>
-                <ul>
-                    <li><a href="settings.php?user_id=<?php echo $user['id']; ?>">Paramètres</a></li>
-                    <li><a href="followers.php?user_id=<?php echo $user['id']; ?>">Mes suiveurs</a></li>
-                    <li><a href="subscriptions.php?user_id=<?php echo $user['id']; ?>">Mes abonnements</a></li>
-                </ul>
+       
+    <?php include 'header.php'; ?>
 
-            </nav>
-        </header>
         <div id="wrapper">
             <aside>
                 <img src="user.jpg" alt="Portrait de l'utilisatrice"/>
