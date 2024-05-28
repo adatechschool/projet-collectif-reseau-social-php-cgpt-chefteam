5f8826280463e3565e1132b0e895243d30a39769
Gulbahar57 <158152207+Gulbahar57@users.noreply.github.com>
2024-05-28 10:58:22 +0200

    subscriptions php done

--- Niveau1/subscriptions.php
+++ Niveau1/subscriptions.php
@@ -43,7 +43,7 @@
                 // Etape 1: récupérer l'id de l'utilisateur
                 $userId = intval($_GET['user_id']);
                 // Etape 2: se connecter à la base de donnée
-                $mysqli = new mysqli("localhost", "root", "root", "socialnetwork");
+                $mysqli = new mysqli("localhost", "root", "", "socialnetwork");
                 // Etape 3: récupérer le nom de l'utilisateur
                 $laQuestionEnSql = "
                     SELECT users.* 
@@ -55,12 +55,19 @@
                 $lesInformations = $mysqli->query($laQuestionEnSql);
                 // Etape 4: à vous de jouer
                 //@todo: faire la boucle while de parcours des abonnés et mettre les bonnes valeurs ci dessous 
+
+              
+
+                while ($post = $lesInformations->fetch_assoc())
+                {
+                    /* echo "<pre>" . print_r($post, 1) . "</pre>"; */
                 ?>
                 <article>
                     <img src="user.jpg" alt="blason"/>
-                    <h3>Alexandra</h3>
-                    <p>id:654</p>                    
-                </article>
+                    <h3><?php echo $post['alias'] ?></h3>
+                    <p><?php echo $post['id'] ?></p>                    
+                </article> 
+                <?php } ?>
             </main>
         </div>
     </body>
